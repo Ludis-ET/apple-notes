e25d562febe11933563130fb5a98cb20f51db9f6
Ludis-et <leulsegedmelaku1020@gmail.com>
2024-03-01 16:55:06 +0300

    Update NotePage component to fetch note by ID from API and enable editing of note body.

--- src/pages/NotePage.jsx
+++ src/pages/NotePage.jsx
@@ -3,17 +3,19 @@ import arrow from "../assets/arrow-left.svg";
 import { useEffect, useState } from "react";
 
 export const NotePage = (props) => {
-  const [note, setNote] = useState(null);
+  const [note, setNote] = useState([]);
   const { id } = useParams();
+  let url = `http://localhost:3001/notes/${id}`;
   useEffect(() => {
     async function handleFetch() {
-      const response = await fetch(`http://localhost:3001/notes/${id}`);
+      const response = await fetch(url);
       const data = await response.json();
-      console.log(data);
       setNote(data);
     }
     handleFetch();
-  }, [id]);
+  }, [url]);
+
+  
   return (
     <div className="note">
       <div className="note-header">
@@ -23,7 +25,16 @@ export const NotePage = (props) => {
           </Link>
         </h3>
       </div>
-      <textarea name="" value={note.body} id="" cols="30" rows="10"></textarea>
+      <textarea
+        name=""
+        value={note.body}
+        onChange={(e) => {
+          setNote({ ...note, body: e.target.value });
+        }}
+        id=""
+        cols="30"
+        rows="10"
+      ></textarea>
     </div>
   );
 };
