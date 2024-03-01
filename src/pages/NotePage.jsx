343480a01a70ae1273d858cce672f81d394f3a4e
Ludis-et <leulsegedmelaku1020@gmail.com>
2024-03-01 16:29:37 +0300

    Update notes to be fetched from a local server.

--- src/pages/NotePage.jsx
+++ src/pages/NotePage.jsx
@@ -1,10 +1,19 @@
 import { useParams, Link } from "react-router-dom";
-import notes from "../assets/data";
 import arrow from "../assets/arrow-left.svg";
+import { useEffect, useState } from "react";
 
 export const NotePage = (props) => {
+  const [note, setNote] = useState(null);
   const { id } = useParams();
-  let note = notes.find((note) => note.id === Number(id));
+  useEffect(() => {
+    async function handleFetch() {
+      const response = await fetch(`http://localhost:3001/notes/${id}`);
+      const data = await response.json();
+      console.log(data);
+      setNote(data);
+    }
+    handleFetch();
+  }, [id]);
   return (
     <div className="note">
       <div className="note-header">
--- src/pages/NotesListPage.jsx
+++ src/pages/NotesListPage.jsx
@@ -1,15 +1,26 @@
-import data from "../assets/data";
+import { useEffect, useState } from "react";
 import { ListItem } from "../components/ListItem";
 
 export const NotesListPage = () => {
+  const [notes, setNotes] = useState([]);
+
+  useEffect(() => {
+    async function handleFetch() {
+      const response = await fetch("http://localhost:3001/notes");
+      const data = await response.json();
+      setNotes(data);
+    }
+    handleFetch();
+  }, []);
+
   return (
     <div className="notes">
       <div className="notes-header">
         <h2 className="notes-title">&#9782; Notes</h2>
-        <p className="notes-count">{data.length}</p>
+        <p className="notes-count">{notes.length}</p>
       </div>
       <div className="notes-list">
-        {data.map((note, index) => (
+        {notes.map((note, index) => (
           <ListItem key={index} note={note} />
         ))}
       </div>
